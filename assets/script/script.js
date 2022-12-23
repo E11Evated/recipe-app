b2b085630f49735b67e7563d8bd6217655d8c680
gorillaRouge <john.schoppmann@yahoo.com>
2022-12-22 20:05:02 -0700

    test

--- assets/script/script.js
+++ assets/script/script.js
@@ -29,12 +29,13 @@ function generateHTML(results) {
     return;
   }
 
-  results.forEach(result => {
+  results.forEach((result, index) => {
+    const itemId = `${result.recipe.label}-${index}`;
     generatedHTML += `
       <div class="item">
           <img src="${result.recipe.image}" alt="">
           <div class="flex-container">
-              <h1 class="title">${result.recipe.label}</h1><span class="badge" style="display: none;">Saved</span>
+              <h1 class="title" data-item-id="${itemId}" data-item-title="${result.recipe.label}" data-item-image="${result.recipe.image}" data-item-url="${result.recipe.url}">${result.recipe.label}</h1><span class="badge" style="display: none;">Saved</span>
              <a class="view-button" href="${result.recipe.url}" target="_blank">View Recipe</a>
           </div>
        <p class="item-data">Calories: ${result.recipe.calories.toFixed(2)}</p>
@@ -45,68 +46,72 @@ function generateHTML(results) {
   });
   searchResultDiv.innerHTML = generatedHTML;
 }
-/* Worked on click. */
-/* var item = $('.search-result');
 
-item.on('click', function(event) {
-  console.log("it worked");
-  const badge = document.querySelector('.badge');
-if (badge) {
-  badge.style.display = 'inline-block';
-}
 
-  // Get the item data
-  const itemId = $(event.target).get(0).dataset.itemId;
-  const item = {
-    title: $(event.target).get(0).dataset.itemTitle,
-    image: $(event.target).get(0).dataset.itemImage,
-    url: $(event.target).get(0).dataset.itemUrl,
-  };
+document.addEventListener('DOMContentLoaded', function() {
+  const items = document.querySelectorAll('.title');
 
-  // Save the item in local storage
-  localStorage.setItem(itemId, JSON.stringify(item));
-}); */
-
-var items = document.querySelectorAll('.item .title');
-
-items.forEach(item => {
-  item.addEventListener('click', function(event) {
-    // Show the badge
-    const badge = event.target.nextElementSibling;
-    badge.style.display = 'inline-block';
-
-    // Get the item data
-    const itemId = event.target.dataset.itemId;
-    const itemData = {
-      title: event.target.dataset.itemTitle,
-      image: event.target.dataset.itemImage,
-      url: event.target.dataset.itemUrl,
-    };
-
-    // Save the item in local storage
-    localStorage.setItem(itemId, JSON.stringify(itemData));
+  searchResultDiv.addEventListener('dblclick', function(event) {
+    if (event.target.classList.contains('title')) {
+      // Show the badge
+      const badge = event.target.nextElementSibling;
+      badge.style.display = 'inline-block';
+  
+      // Get the item data
+      const itemId = event.target.dataset.itemId;
+      const item = {
+        title: event.target.dataset.itemTitle,
+        image: event.target.dataset.itemImage,
+        url: event.target.dataset.itemUrl,
+      };
+  
+      // Save the item in local storage
+      localStorage.setItem(itemId, JSON.stringify(item));
+    }
   });
 });
-const nav = document.querySelector('.navBar');
 
-// Create the dropdown menu element
-const dropdownMenu = document.createElement('ul');
-dropdownMenu.classList.add('dropdown-menu');
+document.addEventListener('DOMContentLoaded', function() {
+  const nav = document.querySelector('.navBar');
 
-// Add the saved items to the dropdown menu
+  // Create the dropdown menu element
+  const dropdownMenu = document.createElement('ul');
+  dropdownMenu.classList.add('dropdown-menu');
+
+  // Add the saved items to the dropdown menu
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    const item = JSON.parse(localStorage.getItem(key));
+
+    const li = document.createElement('li');
+    li.innerHTML = `
+      <a href="${item.url}" target="_blank">
+        <p>${item.title}</p>
+      </a>
+    `;
+    dropdownMenu.appendChild(li);
+  }
+
+  const navTitleTwo = document.querySelector('.navTitle.two');
+
+  navTitleTwo.addEventListener('click', function() {
+    dropdownMenu.classList.toggle('hidden');
+  });
+
+// Append the dropdown menu to the nav bar
+nav.appendChild(dropdownMenu);
+
+// Add the saved items to the navTitleTwo element
 for (let i = 0; i < localStorage.length; i++) {
   const key = localStorage.key(i);
   const item = JSON.parse(localStorage.getItem(key));
 
-  const li = document.createElement('li');
-  li.innerHTML = `
-    <a href="${item.url}" target="_blank">
-      <img src="${item.image}" alt="">
-      <p>${item.title}</p>
-    </a>
+  const a = document.createElement('a');
+  a.innerHTML = `
+    <p>${item.title}</p>
   `;
-  dropdownMenu.appendChild(li);
+  a.href = item.url;
+  a.target = '_blank';
+  navTitleTwo.appendChild(a);
 }
-
-// Append the dropdown menu to the nav bar
-nav.appendChild(dropdownMenu);
+});
